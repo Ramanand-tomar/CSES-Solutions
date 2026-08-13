8fc24f077c96aaa622979c3ae8db2799db0a75de
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-13 12:42:48 +0530

    Add C++ solution for Weird Algorithm

---
+++ Introductory_Problems/Weird_Algorithm/Weird_Algorithm.cpp
@@ -0,0 +1,8 @@
+/*
+ * Problem Name: Weird Algorithm
+ * Problem ID: 1068
+ * Category: Introductory Problems
+ * CSES URL: https://cses.fi/problemset/task/1068
+*/
+
+#include <bits/stdc++.h>using namespace std; int main() {    long long n;    cin >> n;     while (true) {        cout << n << " ";         if (n == 1)            break;         if (n % 2 == 0)            n /= 2;        else            n = 3 * n + 1;    }     cout << '\n';     return 0;}
