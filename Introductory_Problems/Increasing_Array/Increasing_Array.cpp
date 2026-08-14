bbb66be62d9ce73d18b53573d360e2cfd150fd68
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-14 18:54:01 +0530

    Add C++ solution for Increasing Array

--- Introductory_Problems/Increasing_Array/Increasing_Array.cpp
+++ Introductory_Problems/Increasing_Array/Increasing_Array.cpp
@@ -5,4 +5,4 @@
  * CSES URL: https://cses.fi/problemset/task/1094
 */
 
-#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);    int n;    cin>>n;    vector<int>arr(n);    for(int i = 0 ; i < n ; i++){        int x ;        cin>>x;        arr[i] = x;    }    int moves = 0;    for(int i = 1 ; i < n ; i++){        if(arr[i-1] < arr[i]){            move += arr[i-1] - arr[i];            arr[i] = arr[i-1];        }    }    cout<<moves<<"\n";    return 0;}
+#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);    int n;    cin>>n;    vector<int>arr(n);    for(int i = 0 ; i < n ; i++){        int x ;        cin>>x;        arr[i] = x;    }    int moves = 0;    for(int i = 1 ; i < n ; i++){        if(arr[i-1] > arr[i]){            move += arr[i-1] - arr[i];            arr[i] = arr[i-1];        }    }    cout<<moves<<"\n";    return 0;}
