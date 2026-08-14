6b39d84eec69a201452bd5730a44bde814bdf500
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-14 19:23:02 +0530

    Add C++ solution for Permutations

--- Introductory_Problems/Permutations/Permutations.cpp
+++ Introductory_Problems/Permutations/Permutations.cpp
@@ -11,30 +11,30 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n ;
-    cin>>n;
-    if(n < 4 && n > 1){
-        cout<<"NO SOLUTION";
-        return 0;
+    
+    int n;
+    cin >> n;
+    
+    if (n == 1) {
+        cout << "1\n";
+    }
+    else if (n <= 3) {
+        cout << "NO SOLUTION\n";
+    }
+    else if (n == 4) {
+        cout << "3 1 4 2\n";
+    }
+    else {
+        // Print all even numbers in decreasing order
+        for (int i = n - (n % 2); i >= 2; i -= 2) {
+            cout << i << " ";
+        }
+        // Print all odd numbers in decreasing order
+        for (int i = n - 1 + (n % 2); i >= 1; i -= 2) {
+            cout << i << " ";
+        }
+        cout << "\n";
     }
     
-    int x = 1;
-    for(int i = 0 ; i < n ; i++){
-        if(x%2 == 0){
-            cout<<x<<" ";
-        }
- 
-    }
-    x = 1;
-    for(int i = 0 ; i < n ; i++){
-        if(x%2 != 0){
-            cout<<x<<" ";
-        }
-    }
- 
-    
- 
- 
- 
     return 0;
 }
