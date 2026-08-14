32ff7603ad850a15f7228cae04edfce49b8d107c
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-14 18:58:52 +0530

    Add C++ solution for Increasing Array

--- Introductory_Problems/Increasing_Array/Increasing_Array.cpp
+++ Introductory_Problems/Increasing_Array/Increasing_Array.cpp
@@ -3,6 +3,31 @@
  * Problem ID: 1094
  * Category: Introductory Problems
  * CSES URL: https://cses.fi/problemset/task/1094
-*/
+ */
 
-#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);    int n;    cin>>n;    vector<int>arr(n);    for(int i = 0 ; i < n ; i++){        int x ;        cin>>x;        arr[i] = x;    }    int moves = 0;    for(int i = 1 ; i < n ; i++){        if(arr[i-1] > arr[i]){            moves += arr[i-1] - arr[i];            arr[i] = arr[i-1];        }    }    cout<<moves<<"\n";    return 0;}
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    vector<long long> arr(n);  // Use long long for array elements (up to 10^9)
+    
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];  // Simplified input
+    }
+    
+    long long moves = 0;  // Use long long to avoid overflow
+    for(int i = 1 ; i < n ; i++){
+        if(arr[i-1] > arr[i]){
+            moves += arr[i-1] - arr[i];  // Fixed: moves, not move
+            arr[i] = arr[i-1];
+        }
+    }
+    
+    cout << moves << "\n";
+    return 0;
+}
