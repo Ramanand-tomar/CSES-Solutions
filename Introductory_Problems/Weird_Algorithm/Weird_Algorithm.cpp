f5a3ac2f95f53f8d2d7bd01d469ebff02e2631fc
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-14 16:32:05 +0530

    Add C++ solution for Weird Algorithm

--- Introductory_Problems/Weird_Algorithm/Weird_Algorithm.cpp
+++ Introductory_Problems/Weird_Algorithm/Weird_Algorithm.cpp
@@ -5,4 +5,21 @@
  * CSES URL: https://cses.fi/problemset/task/1068
 */
 
-#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);    int n;    cin>>n;    while(n != 1){        cout<<n<<"\n";        if(n%2 == 0){            n /= 2;        }        else{            n = 3*n + 1;        }    }       return 0;}
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll n;
+    cin >> n;
+    while (true) {
+        cout << n << " ";
+        if (n == 1) break;
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+    cout << "\n";
+}
