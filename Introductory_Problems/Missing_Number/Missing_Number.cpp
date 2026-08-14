4c57e28776f15d3ad31d64bb5a8d02b41fb195f3
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-14 18:06:21 +0530

    Add C++ solution for Missing Number

--- Introductory_Problems/Missing_Number/Missing_Number.cpp
+++ Introductory_Problems/Missing_Number/Missing_Number.cpp
@@ -5,4 +5,4 @@
  * CSES URL: https://cses.fi/problemset/task/1083
 */
 
-#include <bits/stdc++.h>using namespace std;using ll = long long;int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);    int n;    cin>>n;    int sum = 0;    for(int i = 0 ; i < n-1 ; i++){        int x;        cin>>x;        sum += x;    }    ll total = (ll)(n*(n+1)/2);    ll ans = total - sum;    cout<<ans<<"\n";        return 0;}
+#include <bits/stdc++.h>using namespace std;using ll = long long;int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);    int n;    cin>>n;    ll sum = 0;    for(int i = 0 ; i < n-1 ; i++){        ll x;        cin>>x;        sum += x;    }    ll total = (ll)(n*(n+1)/2);    ll ans = total - sum;    cout<<ans<<"\n";        return 0;}
