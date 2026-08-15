5385b44b324a59226eb52c0d6a72b0c6364f48c7
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 19:23:18 +0530

    Add C++ solution for Palindrome Reorder

--- Introductory_Problems/Palindrome_Reorder/Palindrome_Reorder.cpp
+++ Introductory_Problems/Palindrome_Reorder/Palindrome_Reorder.cpp
@@ -7,46 +7,64 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+ 
     string str;
-    cin>>str;
-    vector<ll>alpha(26,0);
-    for(ll i = 0 ; i < str.size() ; i++){
-        alpha[str[i]-'A']++;
+    cin >> str;
+ 
+    vector<int> alpha(26, 0);
+ 
+    // Count frequency of each character
+    for (char ch : str) {
+        alpha[ch - 'A']++;
     }
-    int count = 0;
-    for(ll i = 0 ; i < 26 ; i++){
-        if(alpha[i]%2 != 0){
-            count++;
+ 
+    // Count characters with odd frequency
+    int oddCount = 0;
+ 
+    for (int i = 0; i < 26; i++) {
+        if (alpha[i] % 2 != 0) {
+            oddCount++;
         }
     }
-    if(count > 1){
-        cout<<"NO SOLUTION\n";
+ 
+    // A palindrome can have at most one character
+    // with an odd frequency
+    if (oddCount > 1) {
+        cout << "NO SOLUTION\n";
         return 0;
     }
-    string ans(str.size(), ' ');
-    ll start = 0 , end = str.size()-1;
-    for(ll i = 0 ; i < str.size() ; i++){
-        if(alpha[str[i]]%2 == 1){
-            // odd
-            ans[str.size()/2] = str[i];
-            alpha[str[i]]--;
  
+    string ans(str.size(), ' ');
+ 
+    int left = 0;
+    int right = str.size() - 1;
+ 
+    // Put pairs of characters at both ends
+    for (int i = 0; i < 26; i++) {
+ 
+        // Put the odd-frequency character in the middle
+        if (alpha[i] % 2 != 0) {
+            ans[str.size() / 2] = char('A' + i);
+            alpha[i]--;
         }
-        while(alpha[str[i]]>0){
-            ans[start] = ans[end] = str[i];
+ 
+        // Put pairs on both sides
+        while (alpha[i] > 0) {
+            ans[left] = char('A' + i);
+            ans[right] = char('A' + i);
+ 
             left++;
             right--;
-            alpha[str[i]] -= 2;
+ 
+            alpha[i] -= 2;
         }
     }
-    cout<<ans<<"\n";
  
- 
- 
+    cout << ans << '\n';
  
     return 0;
 }
