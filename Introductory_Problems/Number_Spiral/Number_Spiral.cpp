024112c864ad59c8741dd0ae6b251ef5be39f7b0
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 15:24:34 +0530

    Add C++ solution for Number Spiral

--- Introductory_Problems/Number_Spiral/Number_Spiral.cpp
+++ Introductory_Problems/Number_Spiral/Number_Spiral.cpp
@@ -7,7 +7,7 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -16,37 +16,49 @@ int main() {
     cin >> t;
     
     while(t--) {
-        long long y, x;
-        cin >> y >> x;
-        
-        long long m = max(y, x);
-        long long ans;
-        
-        if(m % 2 == 0) {
-            // Even m: spiral goes counter-clockwise from (m,1)
-            if(y == 1) {
-                ans = (m - 1) * (m - 1) + x;
-            } else if(x == m) {
-                ans = (m - 1) * (m - 1) + (m - 1) + y;
-            } else if(y == m) {
-                ans = m * m - (m - x);
-            } else {
-                ans = m * m - (m - 1) - (m - y);
-            }
-        } else {
-            // Odd m: spiral goes clockwise from (1,m)
-            if(x == 1) {
-                ans = (m - 1) * (m - 1) + y;
-            } else if(y == m) {
-                ans = (m - 1) * (m - 1) + (m - 1) + (m - x);
-            } else if(y == 1) {
-                ans = m * m - (x - 1);
-            } else {
-                ans = m * m - (m - 1) - (m - y);
-            }
+        long long Y, X;
+        cin >> Y >> X;
+        if (Y > X) {
+        // Compute the area of the inner square
+        ll ans = (Y - 1) * (Y - 1);
+        ll add = 0;
+ 
+        // Check parity of Y to determine if numbers are in
+        // increasing or decreasing order
+        if (Y % 2 != 0) {
+            // Add X to the area if Yth row is odd
+            add = X;
         }
+        else {
+            // Add 2*Y - X to the area if Yth row is even
+            add = 2 * Y - X;
+        }
+        // Print the final result
+        cout << ans + add << "\n";
+    }
+    // If X is greater than or equal to Y, implying Xth
+    // column is the outer boundary
+    else {
+ 
+        // Compute the area of the inner square
+        ll ans = (X - 1) * (X - 1);
+        ll add = 0;
+ 
+        // Check parity of X to determine if numbers are in
+        // increasing or decreasing order
+        if (X % 2 == 0) {
+            // Add Y to the area if Xth column is even
+            add = Y;
+        }
+        else {
+            // Add 2*X - Y to the area if Xth column is odd
+            add = 2 * X - Y;
+        }
+        // Print the final result
+        cout << ans + add << "\n";
+    }
         
-        cout << ans << "\n";
+ 
     }
     
     return 0;
