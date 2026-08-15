3fa00491b16416d03ce5617750130eceef407943
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 16:30:58 +0530

    Add C++ solution for Two Knights

--- Introductory_Problems/Two_Knights/Two_Knights.cpp
+++ Introductory_Problems/Two_Knights/Two_Knights.cpp
@@ -9,33 +9,19 @@
 using namespace std;
  
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n ;
-    cin>>n;
-    int sum = 0;
-    vector<int>dx{1,1,2,2,-1,-1,-2,-2};
-    vector<int>dy{2,-2,1,-1,2,-2,1,-1};
-    
-    for(int k = 1 ; k<= n ; k++){
-        int sum = 0;
-        int total = (k*k)*(k*k-1)/2;
-        for(int i = 0 ; i < k ; i++){
-            for(int j = 0 ; j < k ; j++){
-                for(int m = 0 ; m < 8 ; m++){
-                    int x = dx[m];
-                    int y = dy[m];
-                    if(x>=0 &&x < k && y >= 0 && y<k){
-                        sum++;
-                    }
-                }
-            }
-        }
-        int ans = total - sum/2;
-        cout<<ans<<"\n";
+    int n;
+    cin >> n;
+ 
+    for (long long k = 1; k <= n; k++) {
+ 
+        long long total = k * k * (k * k - 1) / 2;
+ 
+        long long attacking = 4 * (k - 1) * (k - 2);
+ 
+        long long answer = total - attacking;
+ 
+        cout << answer << '\n';
     }
- 
- 
  
     return 0;
 }
