744309bfd021dd0e91815c72232e05128a63af0d
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 15:19:19 +0530

    Add C++ solution for Number Spiral

--- Introductory_Problems/Number_Spiral/Number_Spiral.cpp
+++ Introductory_Problems/Number_Spiral/Number_Spiral.cpp
@@ -19,38 +19,30 @@ int main() {
         long long y, x;
         cin >> y >> x;
         
-        long long n = max(y, x);
+        long long m = max(y, x);
         long long ans;
         
-        if(n % 2 == 0) {
-            // For even n, the maximum (n*n) is at (1, 1)
+        if(m % 2 == 0) {
+            // Even m: spiral goes counter-clockwise from (m,1)
             if(y == 1) {
-                // Top row: decreases from right to left
-                ans = n * n - (x - 1);
-            } else if(x == n) {
-                // Right column: decreases from top to bottom
-                ans = n * n - (n - 1) - (y - 1);
-            } else if(x == 1) {
-                // Left column: increases from top to bottom
-                ans = (n - 1) * (n - 1) + y;
+                ans = (m - 1) * (m - 1) + x;
+            } else if(x == m) {
+                ans = (m - 1) * (m - 1) + (m - 1) + y;
+            } else if(y == m) {
+                ans = m * m - (m - x);
             } else {
-                // Bottom row: increases from right to left
-                ans = (n - 1) * (n - 1) + (n - 1) + (n - x);
+                ans = m * m - (m - 1) - (m - y);
             }
         } else {
-            // For odd n, the maximum (n*n) is at (n, 1)
-            if(y == n) {
-                // Bottom row: decreases from left to right
-                ans = n * n - (x - 1);
-            } else if(x == 1) {
-                // Left column: decreases from bottom to top
-                ans = n * n - (n - 1) - (n - y);
+            // Odd m: spiral goes clockwise from (1,m)
+            if(x == 1) {
+                ans = (m - 1) * (m - 1) + y;
+            } else if(y == m) {
+                ans = (m - 1) * (m - 1) + (m - 1) + (m - x);
             } else if(y == 1) {
-                // Top row: increases from left to right
-                ans = (n - 1) * (n - 1) + x;
+                ans = m * m - (x - 1);
             } else {
-                // Right column: increases from top to bottom
-                ans = (n - 1) * (n - 1) + (n - 1) + y;
+                ans = m * m - (m - 1) - (m - y);
             }
         }
         
