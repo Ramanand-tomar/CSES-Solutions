773ed4a16b278d342671a223d81abad6a1ba3d61
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 18:15:11 +0530

    Add C++ solution for Trailing Zeros

---
+++ Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem Name: Trailing Zeros
+ * Problem ID: 1618
+ * Category: Introductory Problems
+ * CSES URL: https://cses.fi/problemset/task/1618
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n ;
+    cin>>n;
+    int ans = 0;
+    while(n>5){
+        n = n/5;
+        ans += n;
+    }
+    cout<<ans<<"\n";
+ 
+ 
+ 
+    return 0;
+}
