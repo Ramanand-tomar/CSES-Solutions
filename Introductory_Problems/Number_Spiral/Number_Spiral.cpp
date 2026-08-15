a6e8aedc41572d1c5f5bab7654c26ac97fc32284
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 15:16:28 +0530

    Add C++ solution for Number Spiral

--- Introductory_Problems/Number_Spiral/Number_Spiral.cpp
+++ Introductory_Problems/Number_Spiral/Number_Spiral.cpp
@@ -19,49 +19,48 @@ int main() {
         long long y, x;
         cin >> y >> x;
         
-        // Find which layer/ring this point belongs to
-        long long layer = max(y, x);
-        long long max_val = layer * layer;
-        long long min_val = (layer - 1) * (layer - 1) + 1;
+        // Find the maximum coordinate
+        long long n = max(y, x);
         
-        // If we're on the top row or left column (when layer is odd)
-        if(layer % 2 == 1) {
-            // Top row: values increase from left to right
+        long long ans;
+        
+        // The spiral fills layer by layer
+        // In layer n, the maximum value is n*n
+        // The minimum value is (n-1)*(n-1) + 1
+        
+        if(n % 2 == 1) {
+            // Odd layers: the maximum is at bottom-right corner (n, n)
+            if(y == n) {
+                // Bottom row: values decrease as we go left
+                ans = n * n - (n - x);
+            } else if(x == 1) {
+                // Left column: values continue decreasing upwards
+                ans = n * n - (n - 1) - (n - y);
+            } else if(y == 1) {
+                // Top row: values increase from left to right
+                ans = (n - 1) * (n - 1) + x;
+            } else {
+                // Right column: values increase from top to bottom
+                ans = (n - 1) * (n - 1) + (n - 1) + y;
+            }
+        } else {
+            // Even layers: the maximum is at top-left corner (1, 1)
             if(y == 1) {
-                cout << min_val + x - 1 << "\n";
-            }
-            // Left column: values increase from top to bottom (going down)
-            else if(x == 1) {
-                cout << max_val - y + 1 << "\n";
-            }
-            // Right column: values decrease from bottom to top
-            else if(x == layer) {
-                cout << min_val + layer + (layer - y) << "\n";
-            }
-            // Bottom row: values decrease from right to left
-            else if(y == layer) {
-                cout << max_val - (layer - x) << "\n";
+                // Top row: values decrease as we go right
+                ans = n * n - (x - 1);
+            } else if(x == n) {
+                // Right column: values continue decreasing downwards
+                ans = n * n - (n - 1) - (y - 1);
+            } else if(x == 1) {
+                // Left column: values increase from top to bottom
+                ans = (n - 1) * (n - 1) + y;
+            } else {
+                // Bottom row: values increase from right to left
+                ans = (n - 1) * (n - 1) + (n - 1) + (n - x);
             }
         }
-        // If we're on the top row or left column (when layer is even)
-        else {
-            // Top row: values increase from right to left
-            if(y == 1) {
-                cout << max_val - x + 1 << "\n";
-            }
-            // Left column: values decrease from bottom to top
-            else if(x == 1) {
-                cout << min_val + layer - (y - 1) << "\n";
-            }
-            // Right column: values increase from top to bottom
-            else if(x == layer) {
-                cout << min_val + y - 1 << "\n";
-            }
-            // Bottom row: values increase from left to right
-            else if(y == layer) {
-                cout << max_val - (layer - x) << "\n";
-            }
-        }
+        
+        cout << ans << "\n";
     }
     
     return 0;
