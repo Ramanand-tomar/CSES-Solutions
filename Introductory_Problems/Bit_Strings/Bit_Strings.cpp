62a356a4df362301b7dda120592284ac620e1bf2
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 17:51:55 +0530

    Add C++ solution for Bit Strings

---
+++ Introductory_Problems/Bit_Strings/Bit_Strings.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem Name: Bit Strings
+ * Problem ID: 1617
+ * Category: Introductory Problems
+ * CSES URL: https://cses.fi/problemset/task/1617
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using MOD = 1e9 + 7;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    cout<<pow(2,n)%MOD<<"\n";
+ 
+ 
+ 
+    return 0;
+}
