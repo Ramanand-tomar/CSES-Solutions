4957f8bd27b7f80988741a8f59526d88ef6e8aa8
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-08-15 15:17:37 +0530

    Add C++ solution for Number Spiral

--- Introductory_Problems/Number_Spiral/Number_Spiral.cpp
+++ Introductory_Problems/Number_Spiral/Number_Spiral.cpp
@@ -19,44 +19,38 @@ int main() {
         long long y, x;
         cin >> y >> x;
         
-        // Find the maximum coordinate
         long long n = max(y, x);
-        
         long long ans;
         
-        // The spiral fills layer by layer
-        // In layer n, the maximum value is n*n
-        // The minimum value is (n-1)*(n-1) + 1
-        
-        if(n % 2 == 1) {
-            // Odd layers: the maximum is at bottom-right corner (n, n)
-            if(y == n) {
-                // Bottom row: values decrease as we go left
-                ans = n * n - (n - x);
-            } else if(x == 1) {
-                // Left column: values continue decreasing upwards
-                ans = n * n - (n - 1) - (n - y);
-            } else if(y == 1) {
-                // Top row: values increase from left to right
-                ans = (n - 1) * (n - 1) + x;
-            } else {
-                // Right column: values increase from top to bottom
-                ans = (n - 1) * (n - 1) + (n - 1) + y;
-            }
-        } else {
-            // Even layers: the maximum is at top-left corner (1, 1)
+        if(n % 2 == 0) {
+            // For even n, the maximum (n*n) is at (1, 1)
             if(y == 1) {
-                // Top row: values decrease as we go right
+                // Top row: decreases from right to left
                 ans = n * n - (x - 1);
             } else if(x == n) {
-                // Right column: values continue decreasing downwards
+                // Right column: decreases from top to bottom
                 ans = n * n - (n - 1) - (y - 1);
             } else if(x == 1) {
-                // Left column: values increase from top to bottom
+                // Left column: increases from top to bottom
                 ans = (n - 1) * (n - 1) + y;
             } else {
-                // Bottom row: values increase from right to left
+                // Bottom row: increases from right to left
                 ans = (n - 1) * (n - 1) + (n - 1) + (n - x);
+            }
+        } else {
+            // For odd n, the maximum (n*n) is at (n, 1)
+            if(y == n) {
+                // Bottom row: decreases from left to right
+                ans = n * n - (x - 1);
+            } else if(x == 1) {
+                // Left column: decreases from bottom to top
+                ans = n * n - (n - 1) - (n - y);
+            } else if(y == 1) {
+                // Top row: increases from left to right
+                ans = (n - 1) * (n - 1) + x;
+            } else {
+                // Right column: increases from top to bottom
+                ans = (n - 1) * (n - 1) + (n - 1) + y;
             }
         }
         
