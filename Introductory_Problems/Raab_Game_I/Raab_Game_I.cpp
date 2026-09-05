8fb1a34fd9b42225516b9df7c75ce0b49d0488bb
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-09-05 19:01:42 +0530

    Add C++ solution for Raab Game I

---
+++ Introductory_Problems/Raab_Game_I/Raab_Game_I.cpp
@@ -0,0 +1,53 @@
+/*
+ * Problem Name: Raab Game I
+ * Problem ID: 3399
+ * Category: Introductory Problems
+ * CSES URL: https://cses.fi/problemset/task/3399
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n , a , b;
+        cin>>n>>a>>b;
+        if(a+b > n || (a==0) != (b == 0)){
+            cout<<"NO\n";
+            continue;
+        }
+        cout<<"YES\n";
+        vector<int>p1(n),p2(n);
+        itoa(p1.begin() , p1.end(), 1);
+        if(a == 0 && b == 0){
+            p2 = p1;
+        }
+        else{
+            int m = n - (a+b);
+            for(int i = 0 ; i < m ; i++){
+                p2 = (i + a)%m + 1;
+ 
+            }
+            for(int i = m ; i < n ; i++){
+                p2 = (i+1);
+            }
+        }
+        for(auto x : p1){
+            cout<<x<<" ";
+        }
+        cout<<"\n";
+        for(auto x : p2){
+            cout<<x<<" ";
+        }
+        cout<<"\n";
+    }
+ 
+ 
+ 
+ 
+    return 0;
+}
