918162b39170bb3d56f371183b88dbabf49b6ffc
Ramanand Tomar <ramanandtomar1234@gmail.com>
2026-09-05 21:49:35 +0530

    Add C++ solution for Grid Coloring I

---
+++ Introductory_Problems/Grid_Coloring_I/Grid_Coloring_I.cpp
@@ -0,0 +1,49 @@
+/*
+ * Problem Name: Grid Coloring I
+ * Problem ID: 3311
+ * Category: Introductory Problems
+ * CSES URL: https://cses.fi/problemset/task/3311
+ */
+
+in#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int m , n;
+    cin>>n>>m;
+    vector<string>grid(n);
+    for(int i = 0 ; i < n ; i++){
+        string row ;
+        cin>>row;
+        grid[i] = row;
+    }
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            if((i+j)%2 == 0){
+                if(grid[i][j] == 'A'){
+                    grid[i][j] = 'B';
+                }
+                else{
+                    grid[i][j] = 'A';
+                }
+            }
+            else{
+                if(grid[i][j] == 'C'){
+                    grid[i][j] = 'D';
+                }
+                else{
+                    grid[i][j] = 'C';
+                }
+ 
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            cout<<grid[i]<<"\n";
+        }
+    }
+ 
+ 
+    return 0;
+}
